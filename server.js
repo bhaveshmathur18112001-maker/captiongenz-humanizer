b3330284220a09a3aaa06dea914454b4789f280d
Bhavesh Mathur <bhaveshmathur18112001@gmail.com>
2026-05-15 18:11:04 +0530

    fixed railway port

--- server.js
+++ server.js
@@ -267,9 +267,10 @@ START SERVER
 ====================================
 */
 
-const PORT = 5000
+const PORT =
+process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
   console.log(
     `Server Running On Port ${PORT}`
